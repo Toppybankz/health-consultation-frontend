9bcabdb62354f219865b20d4fa30371f4a38e3d8
Tope Light <Toppy@Toppy-MacBook-Pro.local>
2025-07-25 01:44:09 +0100

    Fix: downgraded Tailwind to v3 for CRA compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2563eb", // Blue-600
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
